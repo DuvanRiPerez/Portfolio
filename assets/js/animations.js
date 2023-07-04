72168f56c100c3f2d5963ac552b78216c615787d
DuvanRiPerez <duvanriperez@gmail.com>
2023-07-04 13:54:17 -0500

    all in one, animations scroll, contact me

---
+++ assets/js/animations.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal()
+
+    sr.reveal('.container-card', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    })
+
+    sr.reveal('.imag', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal('.title-skills', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px'
+    })
+
+    sr.reveal('.container-about', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-300px'
+    })
